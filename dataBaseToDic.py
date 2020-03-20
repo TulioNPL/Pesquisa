38c0f70ec5d0546d2ca71a753bc1dd35d024d6a4
TulioNPL <tulionp.lopes@gmail.com>
2020-03-20 18:41:43 -0300

    Cria um dict com uma lista de pontos(coord + hora) pra cada key(id)

--- dataBaseToDic.py
+++ dataBaseToDic.py
@@ -5,24 +5,32 @@
 import csv
 from ponto import Ponto
 
+newDict = {}
+
 with open('roma_5hTo6h_sorted_by_id.csv') as file:
     reader = csv.DictReader(file)
 
     line = reader.__next__() #le a primeira linha
     tag = line['id']
-    print('\n\n\n\n' + str(tag) + ": ", end='')
+
+
+    newDict[tag] = [] #cria uma lista onde serao adicionados os pontos de cada id
     
+    #Ciclo que percorre todas as linhas, le os pontos e os salva nos respectivos ids no Dictionary
     for line in reader:
-        if tag != line['id']: #Quando a tag for modificada, printa nova tag
+        if tag != line['id']: #Quando a tag for modificada, cria uma nova key
             tag = line['id']
-            print('\n\n\n\n' + str(tag) + ": ", end='')
+            newDict[tag] = []
 
         coord = (float(line['lat_y']),float(line['long_x']))
         hour = line['time']
 
+        #Cria uma nova instancia de Ponto
         pnt = {}
         pnt['Hora'] = hour
         pnt['Coord'] = coord
-
         newInstance = Ponto(pnt)
-        print(newInstance, end=',')
+
+        newDict[tag].append(newInstance)
+
+    print(newDict['101'][0])
