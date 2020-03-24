40c60d719680d49c27486d81ff87e5cdf5a57f83
TulioNPL <tulionp.lopes@gmail.com>
2020-03-24 16:27:20 -0300

    Removidos arquivos incorretos

--- mapaPorHora.py
+++ mapaPorHora.py
@@ -19,9 +19,9 @@ for i in range(0,24):
     df2 = df.loc[(df['time'] < upper_limit) & (df['time'] > lower_limit)] #Separa as linhas com horario entre 5h e 5h59min
     #print(df2.long_x.min(), df2.long_x.max(), df2.lat_y.min(), df2.lat_y.max()) #printa os maximos e os minimos de cada eixo do DataFrame
 
-    #df2 = df2.sort_values('time')   #ordena o DataFrame por "data-hora"
-    df2 = df2.sort_values('id')   #ordena o DataFrame por "id"
-    nome = './sortByTime/roma_' + str(i) + 'hTo' + str(i+1) + 'h_sorted_by_id.csv'
+    df2 = df2.sort_values('time')   #ordena o DataFrame por "data-hora"
+    #df2 = df2.sort_values('id')   #ordena o DataFrame por "id"
+    nome = './sortByTime/roma_' + str(i) + 'hTo' + str(i+1) + 'h_sorted_by_time.csv'
     df2.to_csv(nome, index = False) #salva o DataFrame para CSV
 
  ###
