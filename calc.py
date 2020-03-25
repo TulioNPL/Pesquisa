f64d175000f61184661d9ae40fa11e84f37511e5
TulioNPL <tulionp.lopes@gmail.com>
2020-03-24 23:58:10 -0300

    Inicio de implementacao: funcao de histograma da porcentagem de carros

--- calc.py
+++ calc.py
@@ -53,8 +53,48 @@ class calc:
         plt.savefig('./graficos/carros_x_hora.png')
         plt.show()
         
+    def histCarros():
+        nCarros = np.empty(0,dtype=int)
+        listHora = []
+        totalCarros = 0
+
+        for i in range(0,24):
+            horario = str(i) + 'hTo' + str(i+1) + 'h'
+            x = calc.qtdCarros(horario)
+            totalCarros += x
+            listHora.append(i)
+            nCarros = np.append(nCarros,x)
+        listHora.append(24)
+
+        nCarros = (nCarros * 100)/totalCarros 
+
+        fig, ax = plt.subplots()
+
+        '''
+        index = np.arange(len(nCarros))
+        bar_width = 1
+        score_label = np.arange(0,10,1)
+        bar = ax.hist(index,nCarros,bar_width,label='Porcentagem de Carros',align='edge',color='#ffffff',edgecolor='#000000')
+
+        #setting labels
+        ax.set_ylabel('Porcentagem de carros')
+        ax.set_xlabel('Hora do dia')
+
+        #setting axis
+        ax.set_xticks(index)
+        ax.set_xticklabels(listHora,{'fontsize':7})
+        ax.set_yticks(score_label)
+        ax.set_yticklabels(score_label)
+        '''
+        
+        plt.hist(nCarros)
+        plt.show()
+
+
+
 
 
 
 #Driver
-calc.grafCarros()
+#calc.grafCarros()
+calc.histCarros()
