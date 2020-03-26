2095c53be9e4d6aad1e32e2075c8b6285cdf46e4
TulioNPL <tulionp.lopes@gmail.com>
2020-03-25 22:04:49 -0300

    Constroi o heatmap usando Google Maps

--- mapaPorHora.py
+++ mapaPorHora.py
@@ -7,30 +7,63 @@ import datetime as dt
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
-
-for i in range(0,24):
-    lower_limit = dt.datetime(2014,2,4,i,0,0)
-    upper_limit = dt.datetime(2014,2,4,i,59,59)
-
-    df = pd.read_csv('~/Dropbox/my.folder/Pesquisa/Coding/Dados/roma_calibrated.csv')
-    #print(df.loc[df['id'] == 101].long_x.min(), df.loc[df['id'] == 101].long_x.max(), df.loc[df['id'] == 101].lat_y.min(), df.loc[df['id'] == 101].lat_y.max())
-    df['time'] = pd.to_datetime(df['time'])
-
-    df2 = df.loc[(df['time'] < upper_limit) & (df['time'] > lower_limit)] #Separa as linhas com horario entre 5h e 5h59min
-    #print(df2.long_x.min(), df2.long_x.max(), df2.lat_y.min(), df2.lat_y.max()) #printa os maximos e os minimos de cada eixo do DataFrame
+import matplotlib.cm as cm
+from scipy.ndimage.filters import gaussian_filter
+import gmplot
 
 
+df = pd.read_csv('./sortById/roma_12hTo13h_sorted_by_id.csv')
+lat = df['lat_y']
+lng = df['long_x']
+
+mid_lat = (df.lat_y.min() + df.lat_y.max()) / 2
+mid_lng = (df.long_x.min() + df.long_x.max()) / 2
+
+#print(mid_lat,mid_lng)
+
+gmap = gmplot.GoogleMapPlotter(mid_lat,mid_lng,12)
+gmap.heatmap(lat,lng)
+gmap.draw('./graficos/heatmap_12hTo13h.html')
+
+'''
+def my_plot(x,y,s,bins=1000):
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins)
+    heatmap = gaussian_filter(heatmap, sigma=s)
+
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    return heatmap.T, extent
+
+lower_limit = dt.datetime(2014,2,4,6,0,0)
+upper_limit = dt.datetime(2014,2,4,7,59,59)
+
+df = pd.read_csv('~/Dropbox/my.folder/Pesquisa/Coding/Dados/roma_calibrated.csv')
+#print(df.loc[df['id'] == 101].long_x.min(), df.loc[df['id'] == 101].long_x.max(), df.loc[df['id'] == 101].lat_y.min(), df.loc[df['id'] == 101].lat_y.max())
+df['time'] = pd.to_datetime(df['time'])
+
+df2 = df.loc[(df['time'] < upper_limit) & (df['time'] > lower_limit)] #Separa as linhas com horario entre 5h e 5h59min
+#print(df2.long_x.min(), df2.long_x.max(), df2.lat_y.min(), df2.lat_y.max()) #printa os maximos e os minimos de cada eixo do DataFrame
+
+fig, ax = plt.subplots()
+
+img,extent = my_plot(df.long_x,df.lat_y,128)
+ax.imshow(img,extent=extent,origin='lower',cmap=cm.jet)
+ax.set_title('Heatmap')
+plt.show()
+'''
+'''
 BBox = (df2.long_x.min(),df2.long_x.max(),df2.lat_y.min(),df2.lat_y.max())
-ruh_m = plt.imread('map.png') #mapa a ser usado de fundo
+#ruh_m = plt.imread('map.png') #mapa a ser usado de fundo
 
 fig, ax = plt.subplots(figsize= (8,7))
 ax.scatter(df.long_x, df.lat_y, zorder=1, alpha=0.2, c='b', s=10)
-ax.set_title('Ploting all positions')
+ax.set_title('Scatter plot')
 ax.set_xlim(BBox[0],BBox[1])
 ax.set_ylim(BBox[2],BBox[3])
-ax.imshow(ruh_m, zorder= 0, extent= BBox, aspect= 'equal')
+#ax.imshow(ruh_m,zorder= 0, extent= BBox, aspect= 'equal')
 
-plt.savefig('6-7.png')
+plt.savefig('./graficos/6-7.png')
+plt.show()
+'''
 
 
 ###
