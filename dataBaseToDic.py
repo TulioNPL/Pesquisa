059c07a2d6cfc0cb76426f2aef8c156bcd1b8f9d
TulioNPL <tulionp.lopes@gmail.com>
2020-06-05 15:53:56 -0300

    Separa as trips de cada ID usando como parâmetro a velocidade média

--- dataBaseToDic.py
+++ dataBaseToDic.py
@@ -10,8 +10,10 @@ import matplotlib.pyplot as plt
 
 newDict = {}
 maxDelta = timedelta(hours=0,minutes=0,seconds=30)
-timeGaps = [] #lista com os intervalos de tempos entre os pontos
-coordGaps = [] #lista com os intervalos de distancias entre os pontos
+timeGaps = {} #lista com os intervalos de tempos entre os pontos
+coordGaps = {} #lista com os intervalos de distancias entre os pontos
+travels = [] #lista de viagens
+keys = [] #lista dos ids
 
 def convertHaversine(x1,y1,x2,y2):
     R = 6378.137
@@ -21,14 +23,14 @@ def convertHaversine(x1,y1,x2,y2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = R * c
 
-    return d* 1000 #retorna o valor em metros e não Km
+    return d* 1000 #retorna o valor em metros
 
 with open('./sortById/roma_12hTo13h_sorted_by_id.csv') as file:
     reader = csv.DictReader(file)
 
     line = reader.__next__() #le a primeira linha
     tag = line['id']
-
+    keys.append(line['id'])
 
     newDict[tag] = [] #cria uma lista onde serao adicionados os pontos de cada id
     
@@ -37,6 +39,7 @@ with open('./sortById/roma_12hTo13h_sorted_by_id.csv') as file:
         if tag != line['id']: #Quando a tag for modificada, cria uma nova key
             tag = line['id']
             newDict[tag] = []
+            keys.append(line['id'])
 
         coord = (float(line['lat_y']),float(line['long_x']))
         hour = line['time']
@@ -49,9 +52,12 @@ with open('./sortById/roma_12hTo13h_sorted_by_id.csv') as file:
         
         newDict[tag].append(newInstance)
 
+    #for que percorre cada key no dict
     for key,value in newDict.items():
-        #print(key, end='\n')
+        timeGaps[key] = []
+        coordGaps[key] = []
 
+        #for que percorre cada valor presente na key
         for i in range(len(value)-1):
             point1 = value[i]
             point2 = value[i+1]
@@ -65,9 +71,36 @@ with open('./sortById/roma_12hTo13h_sorted_by_id.csv') as file:
             pointTime2 = datetime.strptime(pointTime2,'%Y-%d-%m %H:%M:%S')
 
             timeGap = pointTime2-pointTime
-            timeGaps.append(timeGap.total_seconds())
+            timeGaps[key].append(timeGap.total_seconds())
 
-            coordGaps.append(convertHaversine(pointCoord1[0],pointCoord1[1],pointCoord2[0],pointCoord2[1]))
+            coordGaps[key].append(convertHaversine(pointCoord1[0],pointCoord1[1],pointCoord2[0],pointCoord2[1]))
 
-    for i in coordGaps:
-        print(i)
+
+    separator = {} #dict em que cada chave contera uma lista com os indices onde as separações devem ser feitas
+
+    #ciclo para gerar os indices onde as viagens de cada key serão divididas
+    for key in keys:
+        flag = False #Se true, entao o tempo decorrido permite a criacao de uma nova trip
+        tempo = 0
+        separator[key] = []
+
+
+        if(len(coordGaps[key]) > 0): #confere se o vetor não está vazio
+            for i in range(0,len(coordGaps[key])-1): 
+                if(timeGaps[key][i] != 0): #confere se o tempo de gravação entre dois pontos não é zero
+                    if(coordGaps[key][i]/timeGaps[key][i] < 0.1):
+                        tempo += timeGaps[key][i]
+                    else:
+                        if(flag):
+                            separator[key].append(i)
+                            flag = False
+                            tempo = 0
+                        else:
+                            tempo = 0
+                            flag = False
+
+                    if(tempo > 15.0):
+                        flag = True
+
+        print('\n'+ key + ': ', end='')
+        print(separator[key])
